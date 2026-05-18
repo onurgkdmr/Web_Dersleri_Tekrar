2ae3b26ff69e2af697989f22e649d4066ff745be
onurg <onurg@DESKTOP-5USBUR7>
2026-05-18 15:41:26 +0300

    Razor konusu işlendi.

--- Ders2_MVC_Razor/Controllers/AnasayfaController.cs
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -12,6 +12,18 @@ namespace Ders2_MVC_Razor.Controllers
 
         public IActionResult Menu()
         {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+            if (DateTime.Now.Hour < 12)
+            {
+                ViewBag.mesaj = "Günaydın";
+            }
+            int x = 90;
+            int[] sayilar = new int[10];
+
             return View();
         }
     }
