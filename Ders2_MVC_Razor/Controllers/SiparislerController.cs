a600142f7a05cf68a3b4c5a889fc7841a26ee563
onurg <onurg@DESKTOP-5USBUR7>
2026-05-18 16:29:15 +0300

    Ödev yapıldı.

--- Ders2_MVC_Razor/Controllers/SiparislerController.cs
+++ Ders2_MVC_Razor/Controllers/SiparislerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ders2_MVC_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ders2_MVC_Razor.Controllers
 {
@@ -11,7 +12,54 @@ namespace Ders2_MVC_Razor.Controllers
             //Kargo maliyeti 0 ise müşteri kargo maliyeti kısmına ücretsiz yazacak şekilde gösteriniz->HTML içinde razor teknolojisi kullanarak yapınız
             //Siparişler için model oluşturunuz.
 
-            return View();
+            List<SiparisListesiModel> siparisListesi = new List<SiparisListesiModel>();
+            
+            SiparisListesiModel siparisEdilenUrun = new SiparisListesiModel();
+
+            siparisEdilenUrun.Id = 1;
+            siparisEdilenUrun.UrunAdi = "Webcam";
+            siparisEdilenUrun.SiparisTarihi = new DateTime(2026, 1, 10);
+            siparisEdilenUrun.MusteriAdiSoyadi = "Serhat Demir";
+            siparisEdilenUrun.Fiyat = 1249.99;
+            siparisEdilenUrun.KargoMaliyeti = 80;
+            siparisEdilenUrun.TeslimEdildiMi = true;
+
+            siparisListesi.Add(siparisEdilenUrun);
+
+            siparisEdilenUrun = new SiparisListesiModel();
+            siparisEdilenUrun.Id = 2;
+            siparisEdilenUrun.UrunAdi = "Powerbank";
+            siparisEdilenUrun.SiparisTarihi = new DateTime(2026, 2, 10);
+            siparisEdilenUrun.MusteriAdiSoyadi = "Mehmet Akın";
+            siparisEdilenUrun.Fiyat = 1449.99;
+            siparisEdilenUrun.KargoMaliyeti = 0;
+            siparisEdilenUrun.TeslimEdildiMi = true;
+
+            siparisListesi.Add(siparisEdilenUrun);
+
+            siparisEdilenUrun = new SiparisListesiModel();
+            siparisEdilenUrun.Id = 3;
+            siparisEdilenUrun.UrunAdi = "Akıllı Saat";
+            siparisEdilenUrun.SiparisTarihi = new DateTime(2026, 3, 10);
+            siparisEdilenUrun.MusteriAdiSoyadi = "Hande Vural";
+            siparisEdilenUrun.Fiyat = 6549.99;
+            siparisEdilenUrun.KargoMaliyeti = 100;
+            siparisEdilenUrun.TeslimEdildiMi = false;
+
+            siparisListesi.Add(siparisEdilenUrun);
+
+            siparisEdilenUrun = new SiparisListesiModel();
+            siparisEdilenUrun.Id = 4;
+            siparisEdilenUrun.UrunAdi = "AirPods";
+            siparisEdilenUrun.SiparisTarihi = new DateTime(2026, 4, 10);
+            siparisEdilenUrun.MusteriAdiSoyadi = "Yavuz Kara";
+            siparisEdilenUrun.Fiyat = 8999.99;
+            siparisEdilenUrun.KargoMaliyeti = 120;
+            siparisEdilenUrun.TeslimEdildiMi = true;
+
+            siparisListesi.Add(siparisEdilenUrun);
+
+            return View(siparisListesi);
         }
     }
 }
---
+++ Ders2_MVC_Razor/Models/SiparisListesiModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Ders2_MVC_Razor.Models
+{
+    public class SiparisListesiModel
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public DateTime SiparisTarihi { get; set; }
+        public string MusteriAdiSoyadi { get; set; }
+        public double Fiyat { get; set; }
+        public double KargoMaliyeti { get; set; }
+        public bool TeslimEdildiMi { get; set; }
+    }
+}
