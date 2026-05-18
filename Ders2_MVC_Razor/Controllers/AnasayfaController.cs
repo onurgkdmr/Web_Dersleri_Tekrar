8bf375fb1331a3ccde0d8693e6be14281139d2ed
onurg <onurg@DESKTOP-5USBUR7>
2026-05-18 15:36:34 +0300

    Razor konusu işlendi.

---
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders2_MVC_Razor.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Razor => HTML sayfasında C# kodları yazmak için kullanılan bir teknolojidir.
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
