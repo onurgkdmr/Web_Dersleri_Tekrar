8b775d4babeeb06ae06705beed6840bc77cab8fb
onurg <onurg@DESKTOP-5USBUR7>
2026-05-20 00:13:40 +0300

    Layout işlendi.

---
+++ Ders3_1_Layout/Controllers/İletisimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders3_1_Layout.Controllers
+{
+    public class İletisimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
