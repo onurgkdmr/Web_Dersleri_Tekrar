a98ac2e3eb608408dc67640108c58f0c2585c971
onurg <onurg@DESKTOP-5USBUR7>
2026-05-21 19:00:53 +0300

    katrmanlı mimariler

---
+++ Ders4_1_DLL/EntitiesTablolar/Kategoriler.cs
@@ -0,0 +1,8 @@
+﻿namespace Ders4_1_DLL.EntitiesTablolar;
+
+public class Kategoriler
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+
+}
---
+++ Ders4_1_Web/Controllers/UrunlerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders4_1_Web.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
